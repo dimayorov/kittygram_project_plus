1140334ad2e99c95ffd20f7bd6d70853fc7cc8c5
Дмитрий Майоров <dimayorov@yandex.ru>
2025-10-07 11:17:25 +0300

    Add achievements for cats

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Achievement, Cat, Owner
+from .models import Achievement, AchievementCat, Cat, Owner
 
 
 class AchievementSerializer(serializers.ModelSerializer):
@@ -11,12 +11,37 @@ class AchievementSerializer(serializers.ModelSerializer):
 
 
 class CatSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField(read_only=True)
-    achievements = AchievementSerializer(read_only=True, many=True)
+    # owner = serializers.StringRelatedField(read_only=True)
+    achievements = AchievementSerializer(many=True, required=False)
 
     class Meta:
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+
+    def create(self, validated_data):
+        # Если в исходном запросе не было поля achievements
+        if 'achievements' not in self.initial_data:
+            # То создаём запись о котике без его достижений
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        # Иначе делаем следующее
+        # Уберём список достижений из словаря validated_data и сохраним его
+        achievements = validated_data.pop('achievements')
+
+        # Создадим нового котика пока без достижений, данных нам достаточно
+        cat = Cat.objects.create(**validated_data)
+
+        # Для каждого достижения из списка достижений
+        for achievement in achievements:
+            # Создадим новую запись или получим существующий экземпляр из БД
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement)
+            # Поместим ссылку на каждое достижение во вспомогательную таблицу
+            # Не забыв указать к какому котику оно относится
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat)
+        return cat
 
 
 class OwnerSerializer(serializers.ModelSerializer):
